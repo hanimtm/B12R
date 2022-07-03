d83b75f0612905c88c99e9d687ebe2b69a1236d4
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-07-03 08:54:35 +0300

    first push contract

---
+++ ahcec_hr_grade/__manifest__.py
@@ -0,0 +1,27 @@
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+{
+    'name': "HR Grade",
+    'summary': """ HR Grade""",
+    'description': """
+         HR Grade
+    """,
+    'author': 'ahcec',
+    'website': 'http://www.ahcec.com',
+    'category': 'HR',
+    'version': '1.0',
+    # 'depends': ['ahcec_hr'],
+    'depends': ['hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_grade_view.xml',
+    ],
+    'price': 10,
+    'currency': 'EUR',
+    'demo': [],
+    'images': [
+        'static/description/main_screen.jpg'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
--- hr_contract/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hr_contract
-from . import hr
-# from . import hr_payroll
-# from . import hr_accural
